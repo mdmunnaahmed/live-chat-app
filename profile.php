b886ef013c0082a68f1d4c21c4e433bcac722838
mu2na <msdmunna77@gmail.com>
2022-03-21 00:39:42 +0600

    search bar dynamic

--- profile.php
+++ profile.php
@@ -64,25 +64,14 @@ include_once "header.php"
         const chatList = document.querySelector(".chat-list");
         const searchBar = document.querySelector(".search-wrapper input");
 
-        setInterval(() => {
-            let xhr = new XMLHttpRequest();
-            xhr.open("GET", "php/profile2.php", true);
-
-            xhr.onload = () => {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        let data = xhr.response;
-                        chatList.innerHTML = data;
-                    }
-                }
-            };
-            xhr.send();
-        }, 1000);
-
-
         searchBar.onkeyup = () => {
-            let xhr = new XMLHttpRequest();
             let searchTerm = searchBar.value;
+            if (searchTerm != "") {
+                searchBar.classList.add("active");
+            } else {
+                searchBar.classList.remove("active");
+            }
+            let xhr = new XMLHttpRequest();
             xhr.open("POST", "php/search-user.php", true);
 
             xhr.onload = () => {
@@ -96,6 +85,23 @@ include_once "header.php"
             xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
             xhr.send("searchTerm=" + searchTerm);
         }
+
+        setInterval(() => {
+            let xhr = new XMLHttpRequest();
+            xhr.open("GET", "php/profile2.php", true);
+
+            xhr.onload = () => {
+                if (xhr.readyState === XMLHttpRequest.DONE) {
+                    if (xhr.status === 200) {
+                        let data = xhr.response;
+                        if (!searchBar.classList.contains("active")) {
+                            chatList.innerHTML = data;
+                        }
+                    }
+                }
+            };
+            xhr.send();
+        }, 1000);
     </script>
 
 </body>
